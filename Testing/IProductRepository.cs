4f9004ec3ed6322897ffefef00b85d49605ad3a3
A.J. Miller <ajmiller1206@gmail.com>
2022-10-20 14:40:36 -0400

    added UpdateProduct

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using Testing.Models;
 
 namespace Testing
@@ -8,5 +9,6 @@ namespace Testing
     {
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
     }
 }
